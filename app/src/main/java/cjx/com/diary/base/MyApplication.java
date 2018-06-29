ada60dc8a40387650e88a56b8e5927b17c6b2b90
bear <zz_caogehao0@126.com>
2018-06-29 11:08:45 +0800

    集成leakCanary

--- app/src/main/java/cjx/com/diary/base/MyApplication.java
+++ app/src/main/java/cjx/com/diary/base/MyApplication.java
@@ -1,9 +1,13 @@
 package cjx.com.diary.base;
 
 import android.app.Application;
+import android.os.Build;
+
+import com.squareup.leakcanary.LeakCanary;
 
 import org.greenrobot.greendao.database.Database;
 
+import cjx.com.diary.BuildConfig;
 import cjx.com.diary.db.MyOpenHelper;
 import cjx.com.diary.mode.weight.DaoMaster;
 import cjx.com.diary.mode.weight.DaoSession;
@@ -22,6 +26,15 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //内存泄漏检测(5.0之前的手机会crash)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                // This process is dedicated to LeakCanary for heap analysis.
+                // You should not init your app in this process.
+                return;
+            }
+            LeakCanary.install(this);
+        }
         INSTANCE = this;
         DaoMaster.DevOpenHelper help = new MyOpenHelper(this, ENCRYPTED ? "bear-db-encrypted" : "bear-db");
         Database db = ENCRYPTED ? help.getEncryptedWritableDb("admin") : help.getWritableDb();
